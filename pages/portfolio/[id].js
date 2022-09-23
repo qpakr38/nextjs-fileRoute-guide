5a3c34acb8bfa9b60da2293f67bb4a0b52391434
qpakr38 <zczc0819@gmail.com>
2022-09-23 13:28:50 +0900

    FEAT: 동적경로 세그먼트 데이터 추출하기

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,4 +1,11 @@
+import {useRouter} from "next/router";
+
 const PortfolioDetailPage = () => {
+    const router = useRouter();
+
+    console.log(router.pathname);
+    console.log(router.query);
+
     return (
         <div>
             <h1>The Portfolio Detail Page</h1>
