b2e488a5f34e03aa725e9769956a5ccb2c4c57b1
qpakr38 <zczc0819@gmail.com>
2022-09-23 13:18:22 +0900

    STYLE : 띄어쓰기 포맷 변경

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,9 +1,9 @@
 const PortfolioDetailPage = () => {
-  return(
-      <div>
-          <h1>The Portfolio Detail Page</h1>
-      </div>
-  )
+    return (
+        <div>
+            <h1>The Portfolio Detail Page</h1>
+        </div>
+    )
 };
 
 export default PortfolioDetailPage;
