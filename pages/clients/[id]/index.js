c6d7a10c14c86b23f0eecd47be7303246776f6ef
qpakr38 <zczc0819@gmail.com>
2022-09-23 14:20:11 +0900

    FEAT : router.push() 사용하기

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,17 @@
+import {router} from "next/client";
+
 const ClientProjectsPage = () => {
+    const loadProjectHandler=()=>{
+        //load data...
+        router.push({
+            pathname: '/clients/[id]/[clientProjectId]',
+            query: {id:'max',clientProjectId: 'projectA'}
+        });
+    };
     return (
         <div>
             <h1>The Projects of a Given Client</h1>
+            <button onClick={loadProjectHandler}>Load Projects A</button>
         </div>
     )
         ;
